1ffd830181b4692de1e2f64f1bdd4b2f0b98a696
vadyk <vadykdeejay@gmail.com>
2026-03-08 10:42:19 +0100

    greeting changed

--- src/app/features/greeting/greeting-page/greeting-page.ts
+++ src/app/features/greeting/greeting-page/greeting-page.ts
@@ -60,9 +60,9 @@ export class GreetingPage implements OnInit {
    */
   get greeting(): string {
     const hour = new Date().getHours();
-    if (hour < 12) return 'Good morning';
-    if (hour < 18) return 'Good afternoon';
-    return 'Good evening';
+    if (hour < 12) return 'Good morning,';
+    if (hour < 18) return 'Good afternoon,';
+    return 'Good evening,';
   }
 
   /**
